53173c35d0ffec159341c8700e7747f08aa62e2e
marvoqs <marvoqs@gmail.com>
2020-12-29 17:08:08 +0100

    add wrapper styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import QuestionCard from './components/QuestionCard';
 import { QuestionState, Difficulty } from './API';
 
 // Styles
-import { GlobalStyle } from './App.styles';
+import { GlobalStyle, Wrapper } from './App.styles';
 
 export type AnswerObject = {
   question: string;
@@ -78,7 +78,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div className='App'>
+      <Wrapper>
         <h1>REACT QUIZ</h1>
         {(gameOver || userAnswers.length === TOTAL_QUESTIONS) && (
           <button className='start' onClick={startTrivia}>
@@ -102,7 +102,7 @@ const App = () => {
             Next Question
           </button>
         )}
-      </div>
+      </Wrapper>
     </>
   );
 };
